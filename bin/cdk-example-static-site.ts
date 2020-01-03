53a37d102ecb591498ebfb9e6d78468831bd84dd
David McElligott <david.mcelligott@mutualofomaha.com>
2020-01-03 00:00:42 -0600

    initial commit

--- bin/cdk-example-static-site.ts
+++ bin/cdk-example-static-site.ts
@@ -3,5 +3,10 @@ import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
 import { CdkExampleStaticSiteStack } from '../lib/cdk-example-static-site-stack';
 
+const env = {
+    region: 'us-east-1',
+    account: '342431730723'
+}
+
 const app = new cdk.App();
-new CdkExampleStaticSiteStack(app, 'CdkExampleStaticSiteStack');
+new CdkExampleStaticSiteStack(app, 'CdkExampleStaticSiteStack', { env: env });
--- lib/cdk-example-static-site-stack.ts
+++ lib/cdk-example-static-site-stack.ts
@@ -1,9 +1,23 @@
 import cdk = require('@aws-cdk/core');
+import staticsite = require('@damc-dev/cdk-static-site');
+import s3deploy = require('@aws-cdk/aws-s3-deployment')
 
 export class CdkExampleStaticSiteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    new staticsite.CdkStaticSite(this, "StaticSite", {
+      domainName: "damc-dev.com",
+      siteSubDomain: "www",
+      staticSiteFiles: s3deploy.Source.asset('site-contents'),
+      staticSiteBucketRemovalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    new staticsite.CdkStaticSitePipeline(this, "StaticSitePipeline". {
+      githubProject: "damc-dev",
+      githubRepo: "test-cdk-static-site",
+      githubOAuthToken: cdk.SecretValue.secretsManager("my-github-token")
+    });
     // The code that defines your stack goes here
   }
 }
